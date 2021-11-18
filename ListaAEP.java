31e31cef6ef52729c096951a904fca0df96c0539
Gustavo481 <77740837+Gustavo481@users.noreply.github.com>
2021-11-17 22:26:50 -0300

    Update and rename Lista.java to ListaAEP.java

--- ListaAEP.java
+++ ListaAEP.java
@@ -1,4 +1,4 @@
-package Lista;
+package ListaAEP;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -11,29 +11,29 @@ public class Lista {
 		try (Scanner read = new Scanner(System.in)) {
 			ArrayList<String> Cadastro = new ArrayList<String>();
 			
-			Cadastro.add("Jõao Paulo");
+			Cadastro.add("JÃµao Paulo");
 			Cadastro.add("Lucas Marcos");
 			Cadastro.add("Carlos Moura");
 			
 			System.out.printf("Percorrer a lista\n");
 			int tam = Cadastro.size();
 			for (int x=0; x<tam; x++) {
-				System.out.printf("Posição %d - %s \n",x, Cadastro.get(x));
+				System.out.printf("PosiÃ§Ã£o %d - %s \n",x, Cadastro.get(x));
 			}
 			
-			System.out.println("Qual posição será excluída ?");
+			System.out.println("Qual posiÃ§Ã£o serÃ¡ excluÃ­da ?");
 			int x = read.nextInt();
 			
 			try {
 				Cadastro.remove(x);
 			} catch (IndexOutOfBoundsException E) {
-				System.out.printf("\n A posição (%s) digitada não consta na lista!!!", E.getMessage());
+				System.out.printf("\n A posiÃ§Ã£o (%s) digitada nÃ£o consta na lista!!!", E.getMessage());
 			}
 			
 			x = 0;
 			
 			for (String pessoas: Cadastro) {
-				System.out.printf("Posição %d - %s \n", x, pessoas);
+				System.out.printf("PosiÃ§Ã£o %d - %s \n", x, pessoas);
 				x++;
 			}
 		}
